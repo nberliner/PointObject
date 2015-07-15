efa29b50f179066f3968fe015f5d05ebb23a9a86
Niklas Berliner <nberliner@users.noreply.github.com>
2015-07-15 14:06:57 +0200

    Code cleanup

--- lib/cluster.py
+++ lib/cluster.py
@@ -26,25 +26,7 @@ from time import sleep
 from utils import *
 from mplWidgets import LassoManager
 
-#def _pickle_method(method):
-#    func_name = method.im_func.__name__
-#    obj = method.im_self
-#    cls = method.im_class
-#    return _unpickle_method, (func_name, obj, cls)
-#
-#def _unpickle_method(func_name, obj, cls):
-#    for cls in cls.mro():
-#        try:
-#            func = cls.__dict__[func_name]
-#        except KeyError:
-#            pass
-#        else:
-#            break
-#    return func.__get__(obj, cls)
-#
-#import copy_reg
-#import types
-#copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+
 
 class C(object):
     def __init__(self, eps, min_samples):
